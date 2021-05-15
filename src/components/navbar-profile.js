4c959aba16ccf6db1e57eb6ea190c21c980e37ce
VenuChoudhary001 <venu.choudhary2000@gmail.com>
2021-05-15 15:06:45 +0530

    updated

--- src/components/navbar-profile.js
+++ src/components/navbar-profile.js
@@ -1,26 +1,27 @@
-import { AppBar, Grid, IconButton, Toolbar } from "@material-ui/core";
+import { AppBar, Grid, IconButton } from "@material-ui/core";
 import React from "react";
 import HomeIcon from "@material-ui/icons/Home";
 function Navbar() {
   return (
-    <AppBar className="navbar"
+    <AppBar
+      className="navbar"
       style={{ backgroundColor: "transparent" }}
       elevation={0}
       position="static"
     >
       {/* <Toolbar  > */}
-        {/* <Container> */}
-        <Grid container>
-          <Grid item xs={6} style={{paddingLeft: "10px", marginTop: "7px"}}>
-            <div className="header mx-1">Farewell 2021</div>
-          </Grid>
-          <Grid item xs={6} style={{textAlign:"right", paddingRight: "12px"}}>
-            <IconButton className="px-0">
-              <HomeIcon style={{ color: "#123456" }} />
-            </IconButton>
-          </Grid>
+      {/* <Container> */}
+      <Grid container>
+        <Grid item xs={6} style={{ paddingLeft: "10px", marginTop: "7px" }}>
+          <div className="header mx-1">Farewell 2021</div>
         </Grid>
-        {/* </Container> */}
+        <Grid item xs={6} style={{ textAlign: "right", paddingRight: "12px" }}>
+          <IconButton className="px-0">
+            <HomeIcon style={{ color: "#123456" }} />
+          </IconButton>
+        </Grid>
+      </Grid>
+      {/* </Container> */}
       {/* </Toolbar> */}
     </AppBar>
   );
